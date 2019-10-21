ba25d33471875413970c2444aaa147f4a6f2bffc
Hans Erik Fjeld <hans.erik.fjeld@embida.no>
2019-10-21 20:18:04 +0200

    Initial packaging

---
+++ setup.py
@@ -0,0 +1,22 @@
+# import os
+from setuptools import setup, find_packages
+
+setup(
+    name='lp_capture',
+    version='0.4.0',
+    description='For capturing LP and other analog audio',
+    author='S3RF',
+    author_email='hanse.fjeld@gmail.com',
+    packages=find_packages(),
+    setup_requires=['wheel'],
+    install_requires=['scipy', 'numpy', 'matplotlib','Serial', 'pyzmq'],
+    include_package_data=True,
+    package_data={
+        'app.static': ['*']
+    },
+    entry_points={
+        'console_scripts': [
+            'lp_capture=app.main:main'
+        ],
+    }
+)
