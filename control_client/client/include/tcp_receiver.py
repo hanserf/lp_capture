b7e226016d703e8f10a6adde914cd4056e6a56bd
Hans Erik Fjeld <hans.erik.fjeld@embida.no>
2020-04-11 21:27:01 +0200

    Writing client for receive matching server

--- control_client/client/include/tcp_receiver.py
+++ control_client/client/include/tcp_receiver.py
@@ -4,25 +4,88 @@ https://stackoverflow.com/questions/48812854/python3-sending-files-via-socket
 Just
 """
 
-import socket, sys, threading
-
+import socket, sys
+import io
+from threading import Thread, BoundedSemaphore
+import os
+import logging
 from time import sleep
 
-host, port = '127.0.0.1', 442
+HEADER_SIZE = 32
+log = logging.getLogger("STREAM-RECEIVE")
+streaming_dict = {"start_time": 0,
+                  "frame_count": 0,
+                  "compression": False,
+                  "buffer_size": 0,
+                  "stream_buffer": 0}
 
 
-class recv_data :
+class BufferReceive():
+    """
+        Result Callback is Uid, Size and File
+        Aquiring
+    """
+
     mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     mysocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    mysocket.connect((host, port))
 
-    def __init__(self):
-        data = self.mysocket.recv(1024)
-        f = open('newfile.webm', 'wb')
-        while data != bytes(''.encode()):
-            #print(data)
-            data = self.mysocket.recv(1024)
-            f.write(data)
+    @staticmethod
+    def __pad_header(header):
+        # This methods pads and encodes header
+        header.ljust(HEADER_SIZE, '-')
+        return header.encode("UTF-8")
 
+    @staticmethod
+    def __strip_header(header):
+        # This method removes padding used to make req/reply static
+        req_header = header.decode('UTF-8')
+        unpadded = ''.join(c for c in req_header if c not in '-')
+        return unpadded.split(':', 1)
 
-re = recv_data()
+    def __init__(self,ip_port_tuple,status_callback=None,result_callback=None):
+        self.mysocket.connect(ip_port_tuple)
+        self.mysocket.setblocking(False)
+        self.mysocket.settimeout(1)
+        self.receive_sem = BoundedSemaphore(value=1)
+        self.result_callback = result_callback
+
+    def __send_header(self):
+        request = "GET_STREAM"
+        header = self.__pad_header(request)
+        self.mysocket.sendall(header)
+
+    def __send_ack(self,uid,size):
+        done_ack = "SUCCESS:{}:{}".format(uid, size)
+        ack_header = self.__pad_header(done_ack)
+        self.mysocket.sendall(ack_header)
+
+    def __receive_stream(self):
+        self.receive_sem.acquire()
+        stream_frame = io.BytesIO()
+        success = False
+        size = None
+        uid = None
+        file_like = None
+        self.__send_header()
+        try:
+            client_ack = self.mysocket.recv(HEADER_SIZE)
+            reply = self.__strip_header(client_ack)
+            uid = reply[0]
+            size = reply[1]
+            data = self.mysocket.recv(int(size))
+            stream_frame.write(data)
+            success = True
+
+        except socket.timeout as e:
+            log.warning("Socket Timeout : {}".format(e))
+
+        finally:
+            if success:
+                stream_frame.seek(0, os.SEEK_END)
+                rx_size = stream_frame.tell()
+                stream_frame.seek(0)
+                if rx_size == int(size):
+                    self.__send_ack(uid,rx_size)
+                    file_like = stream_frame.getvalue()
+            self.result_callback(uid,size,file_like)
+            self.receive_sem.release()
--- recording_server/server/include/tcp_transfer.py
+++ recording_server/server/include/tcp_transfer.py
@@ -25,7 +25,20 @@ class BufferTransfer():
     """
     mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     mysocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-   
+    @staticmethod
+    def __pad_header(header):
+        # This methods pads and encodes header
+        header.ljust(HEADER_SIZE, '-')
+        return header.encode("UTF-8")
+
+    @staticmethod
+    def __strip_header(header):
+        # This method removes padding used to make req/reply static
+        req_header = header.decode('UTF-8')
+        unpadded = ''.join(c for c in req_header if c not in '-')
+        return unpadded.split(':', 1)
+    
+    
     def __init__(self,uid,file_size_tuple, ip_port_tuple, n_clients=1,status_callback=None):
         log.debug()
         self.uid = uid
@@ -33,7 +46,6 @@ class BufferTransfer():
         self.mysocket.bind(ip_port_tuple)
         self.mysocket.listen(n_clients)
         conn, addr = self.mysocket.accept()
-        self.status_callback = status_callback
         self.stream_sem = BoundedSemaphore(value=n_clients)
         for i in range(1,n_clients):
             thread_identifier = (uid,i)
@@ -41,27 +53,36 @@ class BufferTransfer():
             send_thread = Thread(name=thread_name,target=self.__send_file, args=(thread_identifier,file_size_tuple, conn, addr, ))
             send_thread.start()
 
-    @staticmethod
-    def __pad_payload(request):
-        return request.ljust(HEADER_SIZE, '-')
-
+    def __get_client_request(self,conn):
+        active_client = False
+        raw_header = conn.recv(HEADER_SIZE)
+        header = self.__strip_header(raw_header)
+        if header[0] == "GET_STREAM":
+            active_client = True
+        return active_client
 
     def __send_file(self, thread_identifier, file_size_tuple, conn, addr):
         file_like, size = file_size_tuple
-        self.mysocket.setblocking(0)
-        self.mysocket.settimeout(2)
-        with open(file_like, 'rb') as stream_frame:
-            request = "{}:{}".format(thread_identifier[0],size)
-            header = self.__pad_payload(request)
-            conn.send_all(header)
-            data = stream_frame.read(size)
-            conn.sendall(data)
-            ack = conn.recv(HEADER_SIZE)   
-            if ack == "SUCESS:{}".format(request):
-                self.stream_sem.release()
-                log.debug(' THREAD ID : {}, TRANSFER-SUCESSFULL,{}'.format(thread_identifier[1],request))
-                
+        self.mysocket.setblocking(False)
+        self.mysocket.settimeout(1)
+        if self.__get_client_request(conn):
+            with open(file_like, 'rb') as stream_frame:
+                request = "{}:{}".format(thread_identifier[0],size)
+                header = self.__pad_header(request)
+                conn.send_all(header)
+                data = stream_frame.read(int(size))
+                conn.sendall(data)
+                ack = conn.recv(HEADER_SIZE)   
+                if ack == "SUCCESS:{}".format(request):
+                    self.stream_sem.release()
+                    log.debug(' THREAD ID : {}, TRANSFER-SUCCESSFULL,{}'.format(thread_identifier[1],request))
+                    
+        else:
+            self.status_callback("NO_CLIENT:{}".format(thread_identifier[1]))
+            self.stream_sem.release()
+            
     def acknowledge_send(self):
+        sucess = False
         try:
             if not self.stream_sem.acquire(timeout=2):
                 log.info("EXCEPTION, NO ACK - SEMAPHORE TIMEOUT")
